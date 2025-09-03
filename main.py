a6d110a282d20bc6842d205c9f65d4660d932d90
nassaigael <hei.nassai@gmail.com>
2025-09-03 12:06:29 +0300

    update main.py add all routes & Model

--- main.py
+++ main.py
@@ -1,10 +1,38 @@
-from fastapi import FastAPI
-from starlette.responses import Response
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import List, Optional
 
 app = FastAPI()
 
+class Characteristic(BaseModel):
+    max_speed: float
+    max_fuel_capacity: float
+
+class Car(BaseModel):
+    identifier: str
+    brand: str
+    model: str
+    characteristics: Characteristic
+
+cars_db: List[Car] = []
 
 
 @app.get("/ping")
-def hello():
-    return Response(content="pong", status_code=200, media_type="text/plain")
+def ping():
+    return "pong"
+
+@app.post("/cars", status_code=201)
+def create_car(car: Car):
+    cars_db.append(car)
+    return car
+
+@app.get("/cars", response_model=List[Car])
+def get_cars():
+    return cars_db
+
+@app.get("/cars/{id}", response_model=Car)
+def get_car(id: str):
+    for car in cars_db:
+        if car.identifier == id:
+            return car
+    raise HTTPException(status_code=404, detail=f"Car avec l'id '{id}' non trouvé.")
